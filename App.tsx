602ae707e7880e80029d6528bacc7a35e31a9407
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-15 23:42:44 +0000

    #F018, Organizando o projeto com a separacao em arquivos do componente e sua estilizacao

--- App.tsx
+++ App.tsx
@@ -1,34 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do Evento
-      </Text>
-
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
+    <Home />
   )
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16
-  }
-});
