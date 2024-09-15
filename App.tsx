ef0cb527e2def14f8446ab304ae6260055db0468
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-15 22:23:43 +0000

    #F014, entendendo a LogBox

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native"
 export default function App() {
   return (
     <view>
-      <Text>React Native</Text>
+      <Text key={1}>React</Text>
+      <Text key={2}>Native</Text>
     </view>
   )
 }
