2c716273cadffbbd6119237f3c8c75b55398b332
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-15 22:59:45 +0000

    #F015, Estilizando elementos com a propriedade style em cada elemento

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { Text, View } from "react-native"
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <view>
-      <Text key={1}>React</Text>
-      <Text key={2}>Native</Text>
-    </view>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Nome do Evento
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+    </View>
   )
 }
