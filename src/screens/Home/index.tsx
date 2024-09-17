46dd47bb7df8a4ddcbf2f7c5c9f2d4352cf4d213
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-17 02:09:18 +0000

    #F020, Input de dados com o TextInput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native"
+import { View, Text, TextInput } from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
@@ -11,6 +11,12 @@ export function Home() {
         <Text style={styles.eventDate}>
           Sexta, 4 de Novembro de 2022.
         </Text>
+        
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
       </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,5 +15,13 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: '#6B6B6B',
       fontSize: 16
+    },
+    input: {
+      height: 56,
+      borderRadius: 5,
+      padding: 16,
+      fontSize: 16,
+      backgroundColor: '#1F1E25',
+      color: '#FFF',
     }
 });
