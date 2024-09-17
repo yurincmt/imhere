f793429c9c1ae657d2c1e0daddabfa5d80543966
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-17 22:54:36 +0000

    Criacao do componente Participant e sua estilizacao

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,7 @@
 import { View, Text, TextInput, TouchableOpacity } from "react-native"
+
 import { styles } from "./styles"
+import { Participant } from "../components/Participant";
 
 export function Home() {
 
@@ -31,6 +33,10 @@ export function Home() {
             </Text>
           </TouchableOpacity>
         </View>
+
+        <Participant/>
+        <Participant/>
+        <Participant/>
         
       </View>
     )
